e82752592c4fb5320726d61caadc6311d394397d
Ananda Wiradharma <anandawira754@gmail.com>
2020-11-30 18:36:54 +0700

    completed test for calculator.js;

--- js/test/calculator.test.js
+++ js/test/calculator.test.js
@@ -4,6 +4,14 @@ test('Add: should take 2 numbers and return the sum', () => {
   expect(calculator.add(7, 9)).toBe(16);
 });
 
-test('Subtract: should take 2 numbers and return the result of subtraction', () => {
+test('Subtract: should take minuend and subtrahend and return the result of subtraction', () => {
   expect(calculator.subtract(10, 3)).toBe(7);
+});
+
+test('Multiply: should take 2 numbers and return the result of multiplication', () => {
+  expect(calculator.multiply(4, 7)).toBe(28);
+});
+
+test('Divide: should take dividend and divison and return the result of division', () => {
+  expect(calculator.divide(10, 2)).toBe(5);
 });
